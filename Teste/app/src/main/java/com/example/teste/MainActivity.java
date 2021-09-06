516c4e033c04bb8b7b199589127777aba3e7dac2
Sabrina Miranda <sbrina.smiranda@gmail.com>
2021-09-06 11:24:06 -0300

    Adicionando código de teste

---
+++ Teste/app/src/main/java/com/example/teste/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.teste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitRequest(View view) throws IOException {
+        display();
+    }
+
+    private void display() throws IOException {
+        String url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+        TextView request = (TextView) findViewById(R.id.textView);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest Response", response.toString());
+                        request.setText(response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+
+                    }
+                });
+
+                requestQueue.add(objectRequest);
+
+    }
+}
