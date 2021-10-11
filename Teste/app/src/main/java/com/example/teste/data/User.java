fe5b748440bcfc833fc70a6b894285419c89daf7
Mayk B <maykdroid@gmail.com>
2021-10-11 17:47:56 -0300

    Implementado Android Room
    Implementado Android Room funcionando, pegando ids no banco e alimentando favoritos.

--- Teste/app/src/main/java/com/example/teste/data/User.java
+++ Teste/app/src/main/java/com/example/teste/data/User.java
@@ -1,17 +1,18 @@
 package com.example.teste.data;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "user_table")
 public class User {
-    @PrimaryKey(autoGenerate = true)
-    public int uid;
+    @NonNull()
+    @PrimaryKey
+    @ColumnInfo(name = "ID")
+    public String ID;
 
-    @ColumnInfo(name = "first_name")
-    public String firstName;
-
-    @ColumnInfo(name = "last_name")
-    public String lastName;
+    public String getID(){
+        return this.ID;
+    }
 }
--- Teste/app/src/main/java/com/example/teste/data/UserDatabase.java
+++ Teste/app/src/main/java/com/example/teste/data/UserDatabase.java
@@ -1,12 +1,28 @@
 package com.example.teste.data;
 
+import android.content.Context;
+
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {User.class}, version = 1, exportSchema = false)
 public abstract class UserDatabase extends RoomDatabase {
-    public abstract UserDao UserDao();
+    public abstract UserDao userDao();
 
+    private static UserDatabase INSTANCE;
 
+    public static UserDatabase getDbInstance(Context context) {
+
+        if(INSTANCE == null) {
+            synchronized (UserDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            UserDatabase.class, "UserDatabase").allowMainThreadQueries()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
